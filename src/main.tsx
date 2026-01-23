04458ea95c0b1812579e20ca9252690cfd30d9aa
JainAbhi2411 <107537320+JainAbhi2411@users.noreply.github.com>
2026-01-23 00:17:54 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
+import { registerServiceWorker, setupInstallPrompt, checkForUpdates } from './utils/pwa';
+
+// Register Service Worker and setup PWA
+registerServiceWorker().then((registration) => {
+  if (registration) {
+    console.log('[PWA] Service Worker registered successfully');
+    checkForUpdates(registration);
+  }
+}).catch((error) => {
+  console.error('[PWA] Service Worker registration failed:', error);
+});
+
+setupInstallPrompt();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
